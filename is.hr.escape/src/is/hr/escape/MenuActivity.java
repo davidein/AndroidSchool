2eb38e7a176379a832f20255f159ddc44175feb3
Dabbi <davidein@gmail.com>
2013-03-23 10:02:38 +0000

    Small fix.

--- is.hr.escape/src/is/hr/escape/MenuActivity.java
+++ is.hr.escape/src/is/hr/escape/MenuActivity.java
@@ -16,5 +16,7 @@ public class MenuActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.menu);
     }
 }
