657869dd5c667979fe522802c5c64c6a9945ce96
Heiðar Þórðarson <heidarlitli@gmail.com>
2013-03-23 14:08:54 +0000

    Touch handling

--- is.hr.escape/src/is/hr/escape/DrawView.java
+++ is.hr.escape/src/is/hr/escape/DrawView.java
@@ -1,13 +1,14 @@
 package is.hr.escape;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import is.hr.escape.helpers.Orientation;
+import is.hr.escape.logic.Action;
 import is.hr.escape.objects.Car;
+import is.hr.escape.objects.GhostCar;
 
 import java.util.List;
 
@@ -19,12 +20,10 @@ import java.util.List;
 public class DrawView extends View {
     GameHandler handler;
 
-    private Car _movingCar = null;
-    private List<Car> _carList;
+    private GhostCar _ghost = null;
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
         setBackgroundColor(Color.WHITE);
     }
     public void setGameHandler(GameHandler handler) {
@@ -58,15 +57,94 @@ public class DrawView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                Car selected = getCar(x, y);
+                if(selected != null) {
+                    List<Action> actions = handler.getActionsFor(selected);
+                    Point min = new Point(selected.getCol(), selected.getRow()), max = new Point(min);
+
+                    //Find the bounds for the car
+                    for(Action action : actions) {
+                        switch(selected.getOrientation()) {
+                            case Horizontal:
+                                int newCol = selected.getCol() + action.getOffset();
+                                if(newCol < min.x) {
+                                    min.x = newCol;
+                                }
+                                if(newCol > max.x) {
+                                    max.x = newCol;
+                                }
+                                break;
+                            case Vertical:
+                                int newRow = selected.getRow() + action.getOffset();
+                                if(newRow < min.y) {
+                                    min.y = newRow;
+                                }
+                                if(newRow > max.y) {
+                                    max.y = newRow;
+                                }
+                                break;
+                        }
+                    }
+                    min = gridToCoordinates(min.x, min.y);
+                    max = gridToCoordinates(max.x, max.y);
+                    Rect bounds = new Rect(min.x, min.y, max.x, max.y);
+                    _ghost = new GhostCar(selected, gridToCoordinates(selected.getCol(), selected.getRow()), bounds);
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
+                if(_ghost != null) {
+                    Point newPos = new Point(x, y);
+                    if(_ghost.isWithinBounds(newPos)) {
+                        _ghost.setPosition(newPos);
+                    }
+                }
                 break;
             case MotionEvent.ACTION_UP:
-                _movingCar = null;
+                _ghost = null;
                 break;
         }
 
         return true;
     }
 
+    private Point coordinatesToGrid(int x, int y) {
+        int col, row;
+        int columnWidth = getWidth() / handler.getCols() + 1;
+        int rowHeight = getHeight() / handler.getRows() + 1;
+        col = x / columnWidth;
+        row = y / rowHeight;
+        return new Point(col, row);
+    }
+
+    private Point gridToCoordinates(int column, int row) {
+        int x, y;
+        int columnWidth = getWidth() / handler.getCols() + 1;
+        int rowHeight = getHeight() / handler.getRows() + 1;
+
+        x = column * columnWidth;
+        y = row * rowHeight;
+
+        return new Point(x, y);
+    }
+
+    private Car getCar(int x, int y) {
+        Point gridPoint = coordinatesToGrid(x, y);
+
+        List<Car> cars = handler.getCars();
+        for(Car car : cars) {
+            switch(car.getOrientation()) {
+                case Horizontal:
+                    if(gridPoint.y == car.getRow() && gridPoint.x >= car.getCol() && gridPoint.x <= car.getCol() + car.getLength()) {
+                        return car;
+                    }
+                    break;
+                case Vertical:
+                    if(gridPoint.x == car.getCol() && gridPoint.y >= car.getRow() && gridPoint.y <= car.getRow() + car.getLength()) {
+                        return car;
+                    }
+                    break;
+            }
+        }
+        return null;
+    }
 }
---
+++ is.hr.escape/src/is/hr/escape/objects/GhostCar.java
@@ -0,0 +1,40 @@
+package is.hr.escape.objects;
+
+import android.graphics.Point;
+import android.graphics.Rect;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: heidar
+ * Date: 3/23/13
+ * Time: 1:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GhostCar {
+    private int x;
+    private int y;
+    private int id;
+    private Rect bounds;
+
+    public GhostCar(Car car, Point coordinates, Rect bounds) {
+        id = car.getId();
+        x = coordinates.x;
+        y = coordinates.y;
+        this.bounds = bounds;
+    }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+    public int getId() { return id; }
+
+    public void setPosition(Point newPos) {
+        x = newPos.x;
+        y = newPos.y;
+    }
+
+    public boolean isWithinBounds(Point pos) {
+        return pos.x >= bounds.left && pos.x <= bounds.right && pos.y >= bounds.top && pos.y <= bounds.bottom;
+    }
+}
