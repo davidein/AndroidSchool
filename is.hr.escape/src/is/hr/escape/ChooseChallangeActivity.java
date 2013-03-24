83ad9de380411e4fd6916d594aa009536262558f
Dabbi <davidein@gmail.com>
2013-03-24 10:51:04 +0000

    Colors and textures added.

---
+++ is.hr.escape/src/is/hr/escape/ChooseChallangeActivity.java
@@ -0,0 +1,15 @@
+package is.hr.escape;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: David Einarsson
+ * Date: 24.3.2013
+ * Time: 09:56
+ */
+public class ChooseChallangeActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
