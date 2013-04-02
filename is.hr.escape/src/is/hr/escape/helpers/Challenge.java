de84413d829c628ccf0be9d37d37c3b2d9e2dbff
Heiðar Þórðarson <heidarlitli@gmail.com>
2013-04-02 18:01:40 +0000

    Bætti við challenge POJO

---
+++ is.hr.escape/src/is/hr/escape/helpers/Challenge.java
@@ -0,0 +1,17 @@
+package is.hr.escape.helpers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: heidar
+ * Date: 4/2/13
+ * Time: 5:48 PM
+ */
+public class Challenge {
+    public final int id;
+    public final String name;
+
+    public Challenge(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
