6bcaa3e3712e33b1aa0a55474fd0d014a526b68a
Heiðar Þórðarson <heidarlitli@gmail.com>
2013-04-04 22:21:41 +0000

    Comment and refactor

--- is.hr.escape/src/is/hr/escape/activities/ChooseChallengeActivity.java
+++ is.hr.escape/src/is/hr/escape/activities/ChooseChallengeActivity.java
@@ -14,18 +14,21 @@ import is.hr.escape.helpers.SQLHelper;
 import java.util.*;
 
 /**
- * User: David Einarsson
- * Date: 24.3.2013
- * Time: 09:56
+ * This is the level select activity.
+ * The user is presented with all puzzles for a given challenge and can slide between different challenges
  */
 public class ChooseChallengeActivity extends FragmentActivity {
+    /**
+     * The challenges are stored in a ViewPager. Each page contains a list of all puzzles for its challenge.
+     * The current page is stored both when the activity is stopped and when we start a new activity so
+     * that when a user navigates back to this activity, he will return to the same challenge page.
+     *
+     * Note that data is loaded from database every time this activity resumes.
+     */
 
-    private ViewPager pager;
-    private SliderPageAdapter adapter;
-    private int currentPage = 0;
-
-    private final int levelsPerFragment = 9;
-    private Map<Integer, String> fragmentMap = new HashMap<Integer, String>();
+    private ViewPager m_pager;
+    private SliderPageAdapter m_adapter;
+    private int m_currentPage = 0;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,55 +38,45 @@ public class ChooseChallengeActivity extends FragmentActivity {
     @Override
     public void onResume() {
         super.onResume();
-        pager = (ViewPager)findViewById(R.id.pager);
-        adapter = new SliderPageAdapter(getLayoutInflater());
-        pager.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-
-
-            }
-        });
-        pager.setAdapter(adapter);
+        m_pager = (ViewPager)findViewById(R.id.pager);
+        m_adapter = new SliderPageAdapter(getLayoutInflater());
+        m_pager.setAdapter(m_adapter);
 
         SQLHelper helper = new SQLHelper(getBaseContext());
 
         for(Challenge challenge : helper.getAllChallenges()) {
             List<Level> levels = helper.getChallengeLevels(challenge);
-            List<Level> fragmentLevels = null;
-            for(Level level : levels) {
-                if(fragmentLevels == null) {
-                    fragmentLevels = new ArrayList<Level>();
-                }
 
-                fragmentLevels.add(level);
-            }
-            if(fragmentLevels != null) {
-                adapter.addPage(challenge, fragmentLevels);
-            }
+            m_adapter.addPage(challenge, levels);
         }
-        pager.setCurrentItem(currentPage);
-        adapter.notifyDataSetChanged();
+        m_pager.setCurrentItem(m_currentPage);
+        m_adapter.notifyDataSetChanged();
     }
 
     @Override
     public void onSaveInstanceState(Bundle bundle) {
         super.onSaveInstanceState(bundle);
-        bundle.putInt("currentPage", pager.getCurrentItem());
+        bundle.putInt("currentPage", m_pager.getCurrentItem());
     }
 
     @Override
     public void onRestoreInstanceState(Bundle bundle) {
         super.onRestoreInstanceState(bundle);
-        currentPage = bundle.getInt("currentPage", 0);
+        m_currentPage = bundle.getInt("currentPage", 0);
     }
 
+    /**
+     * Callback for when users select a level. Starts a GameActivity on the selected level
+     * @param view
+     */
     public void levelClick(View view) {
+        //The level is stored in the view's tag so that we can determine which level was selected
         Level level = (Level)view.getTag();
         Intent intent = new Intent(this, GameActivity.class);
         intent.putExtra("level", level.getLevel());
         intent.putExtra("levelId", level.getLevelId());
         intent.putExtra("challengeId", level.getChallengeId());
-        currentPage = pager.getCurrentItem();
+        m_currentPage = m_pager.getCurrentItem();
         startActivity(intent);
     }
 }
